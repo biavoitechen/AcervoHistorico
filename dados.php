a9a820a32b7f66c5d16ad5a1ec424d91268f5aa9
voitechen <bia.aran@hotmail.com>
2025-04-08 20:17:43 -0300

    Nomes corrigidos

--- dados.php
+++ dados.php
@@ -13,35 +13,35 @@ $itens = [
         'id' => 2,
         'titulo' => 'Vênus de Milo',
         'categoria' => 'Grécia Antiga',
-        'imagem' => 'imagens/venus_milo.jpg',
+        'imagem' => 'imagens/Venus_milo.jpg',
         'descricao' => 'Escultura famosa da deusa Afrodite, datada de cerca de 130 a.C., encontrada na ilha de Milo, hoje exposta no Louvre.'
     ],
     [
         'id' => 3,
         'titulo' => 'Codex Leicester',
         'categoria' => 'Renascimento',
-        'imagem' => 'imagens/codex.jpg',
+        'imagem' => 'imagens/Codex.jpg',
         'descricao' => 'Manuscrito de Leonardo da Vinci contendo teorias sobre astronomia, geologia e água. Comprado por Bill Gates em 1994.'
     ],
     [
         'id' => 4,
         'titulo' => 'Pedra de Roseta',
         'categoria' => 'Antigo Egito',
-        'imagem' => 'imagens/rosetta.jpg',
+        'imagem' => 'imagens/Rosetta.jpg',
         'descricao' => 'Fragmento de uma estela egípcia que permitiu a decifração dos hieróglifos egípcios. Contém o mesmo texto em três línguas.'
     ],
     [
         'id' => 5,
         'titulo' => 'Esfera Armilar',
         'categoria' => 'Instrumentos Científicos',
-        'imagem' => 'imagens/armilar.jpg',
+        'imagem' => 'imagens/Armilar.jpg',
         'descricao' => 'Antigo modelo do universo usado por astrônomos europeus para representar a posição dos astros ao redor da Terra.'
     ],
     [
         'id' => 6,
         'titulo' => 'Busto de Nefertiti',
         'categoria' => 'Antigo Egito',
-        'imagem' => 'imagens/nefertiti.jpg',
+        'imagem' => 'imagens/Nefertiti.jpg',
         'descricao' => 'Escultura de calcário pintado representando a rainha Nefertiti, símbolo da beleza egípcia, datado de cerca de 1345 a.C.'
     ]
 ];
